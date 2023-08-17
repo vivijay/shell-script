a54226d29f0f7d2255ddd48ae3fb50c145c7285c
Vinit Vijay <vinit@Vinits-MacBook-Air.local>
2023-08-17 23:04:14 +0530

    Some meaningful msg

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 echo " I am learning devops with AWS"
+
+Person1=Vinit
+Person2=Arun
+
+echo "$Person1: Hi Arun how are you ?"
+echo "$Person2: I am good . what about you ?"
+echo "$Person1:Hows is your work going?"
+echo "$Person2: I am learning devops . what about you ? "
