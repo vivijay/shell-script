4af38504696e438a38ed77309392a2e6fbeeac1b
Vinit Vijay <vinit@Vinits-MacBook-Air.local>
2023-08-17 23:08:16 +0530

    Some meaningful msg

---
+++ 03-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+Person1=$1
+Person2=$2
+
+echo "$Person1: Hi Arun how are you ?"
+echo "$Person2: I am good . what about you ?"
+echo "$Person1:Hows is your work going?"
+echo "$Person2: I am learning devops . what about you ? "
