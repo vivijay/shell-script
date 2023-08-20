e65415ec08b473d303e5673848cd6bd1ad9374df
Vinit Vijay <vinit@Vinits-MacBook-Air.local>
2023-08-20 23:57:34 +0530

    Some meaningful msg

--- 11-delete-old-logs.sh
+++ 11-delete-old-logs.sh
@@ -3,7 +3,7 @@
 APP_LOGS_DIR=/home/centos/app-logs
 DATE=$(date +%F)
 LOGSDIR=/home/centos/shell-script.logs
-LOGFILE=$LOGSDIR/$SCRIPTNAME.log
 SCRIPTNAME=$0
+LOGFILE=$LOGSDIR/$SCRIPTNAME.log
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
-echo "$APP_LOGS_DIR"
+echo "$FILES_TO_DELETE"
