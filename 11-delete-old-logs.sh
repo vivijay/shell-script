d084d8e1eb5b732451f9a686c1b3be762648b6b7
Vinit Vijay <vinit@Vinits-MacBook-Air.local>
2023-08-21 00:12:07 +0530

    Some meaningful msg

--- 11-delete-old-logs.sh
+++ 11-delete-old-logs.sh
@@ -7,3 +7,10 @@ SCRIPTNAME=$0
 LOGFILE=$LOGSDIR/$SCRIPTNAME.log
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
 echo "$FILES_TO_DELETE"
+
+while read line
+do
+## what you want to do 
+echo "Deleting $line" &> $LOGFILE 
+rm -rf 
+done >$FILES_TO_DELETE
