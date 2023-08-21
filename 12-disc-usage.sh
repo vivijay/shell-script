d55c4b273ecfb742487b4dcf033de960ff7deaac
Vinit Vijay <vinit@Vinits-MacBook-Air.local>
2023-08-21 17:13:58 +0530

    Added Script

--- 12-disc-usage.sh
+++ 12-disc-usage.sh
@@ -9,9 +9,17 @@ G="\e[32m"
 
 DISC_USAGE=$(df -hT | grep -vE 'tmpfs|Filesystem')
 DISC_USAGE_THRESOLD=1
+MESSAGE=
 
+## IFS means internal field separator is space
 while IFS= read Line
 do
-    echo "Output: $Line"
+    usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
+    partition=$(echo $line | awk '{print $1}')
+    if [ $usage -gt $DISC_USAGE_THRESOLD ]
+    then
+        MESSAGE+= "High disc usage on $partition with $usage"
+    fi
 done <<< $DISC_USAGE
 
+echo "MESSAGE= $MESSAGE"
